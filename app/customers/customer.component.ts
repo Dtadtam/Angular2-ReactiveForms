f13faf5c6657c13b1816d4d865539565834b22e9
DESKTOP-HOME\PC <cyberutimate@gmail.com>
2017-03-20 00:23:56 +0700

    Starter Project including a bit coded to changed from Template-driven form to Reactive forms and validation

---
+++ app/customers/customer.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+// import { NgForm } from '@angular/forms';
+import { FormGroup, /* FormControl, */ FormBuilder, Validators, AbstractControl } from '@angular/forms';
+
+import { Customer } from './customer';
+
+function ratingRange(c: AbstractControl): {[key: string]: boolean} | null {
+    if(c.value != undefined && (isNaN(c.value) || c.value < 1 || c.value > 5)){
+        return {'range': true};
+    }
+    return null;
+}
+
+@Component({
+    selector: 'my-signup',
+    templateUrl: './app/customers/customer.component.html'
+})
+export class CustomerComponent implements OnInit  {
+    customerForm: FormGroup;
+    customer: Customer = new Customer();
+
+    constructor(private fb: FormBuilder){
+
+    }
+
+    ngOnInit(): void {
+        this.customerForm = this.fb.group({
+            firstName: ['', [Validators.required, Validators.minLength(3)]],
+            // lastName:{value: 'n/a', disabled:true},
+            lastName:['', [Validators.required, Validators.maxLength(50)]],
+            email:['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+            phone:'',
+            notification: 'email',
+            rating: ['', ratingRange],
+            sendCatalog: true
+        });
+
+        /* this.customerForm = new FormGroup({
+            firstName: new FormControl(),
+            lastName: new FormControl(),
+            email: new FormControl(),
+            sendCatalog: new FormControl(true)
+        }); */
+    }
+
+    populateTestData(): void {
+        this.customerForm.patchValue({
+            firstName: 'Jack',
+            lastName: 'Harkness',
+            sendCatalog: false
+        });
+    }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+    setNotification(notifyVia: string): void {
+        const phoneControl = this.customerForm.get('phone');
+        if(notifyVia === 'text'){
+            phoneControl.setValidators(Validators.required);
+        }
+        else {
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
+    }
+ }
