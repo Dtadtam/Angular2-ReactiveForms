279fac63889679bd50f91e27fd6a1ff81f389b9b
DESKTOP-HOME\PC <cyberutimate@gmail.com>
2017-03-21 00:31:26 +0700

    To include Reacting change way

--- app/customers/customer.component.ts
+++ app/customers/customer.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 // import { NgForm } from '@angular/forms';
 import { FormGroup, /* FormControl, */ FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
+import 'rxjs/add/operator/debounceTime';
+
 import { Customer } from './customer';
 
 function emailMatcher(c: AbstractControl) {
@@ -32,6 +34,12 @@ function ratingRange(min: number, max: number): ValidatorFn {
 export class CustomerComponent implements OnInit  {
     customerForm: FormGroup;
     customer: Customer = new Customer();
+    emailMessage: string;
+
+    private validationMessages = {
+        required: 'Please enter your email address.',
+        pattern: 'Please enter a valid email address.'
+    }
 
     constructor(private fb: FormBuilder){
 
@@ -58,6 +66,13 @@ export class CustomerComponent implements OnInit  {
             email: new FormControl(),
             sendCatalog: new FormControl(true)
         }); */
+
+        this.customerForm.get('notification').valueChanges
+                        .subscribe(value => this.setNotification(value));
+        
+        const emailControl = this.customerForm.get('emailGroup.email');
+        emailControl.valueChanges.debounceTime(1000).subscribe(value => this.setMessage(emailControl));
+        
     }
 
     populateTestData(): void {
@@ -71,6 +86,14 @@ export class CustomerComponent implements OnInit  {
     save() {
         console.log(this.customerForm);
         console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+    setMessage(c: AbstractControl): void {
+        this.emailMessage = '';
+        if((c.touched || c.dirty) && c.errors) {
+            this.emailMessage = Object.keys(c.errors).map(key =>
+                this.validationMessages[key]).join(' ');
+        }
     }
 
     setNotification(notifyVia: string): void {
